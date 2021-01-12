bdfc85397b5c312037120e82c0bdfb4a8b935dc4
Azhari Marzan <azhari.marzan@gmail.com>
2021-01-12 16:44:36 +0700

    init

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const middlewares = require("./middlewares");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        data: 'Hi there!'
+    });
+});
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+module.exports = app;
