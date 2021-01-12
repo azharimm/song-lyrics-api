3b26702ac2d398fefeaadd7a269027c2fa98bb2f
Azhari Marzan <azhari.marzan@gmail.com>
2021-01-12 16:58:31 +0700

    - routes

--- src/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const indexController = require("../controllers/indexController");
-
-router.get("/", indexController.index);
-
-module.exports = router;
