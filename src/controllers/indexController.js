d50ada3deda92320700a0ddb6654ebb1f182b33e
Azhari Marzan <azhari.marzan@gmail.com>
2021-01-12 18:27:28 +0700

    ^ update

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -28,7 +28,7 @@ exports.hotLyrics = async (req, res) => {
     const htmlResult = await request.get({
         uri: `${process.env.BASE_URL}`,
         headers: {
-            'User-Agent': 'Request-Promise'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36'
         },
     });
     const $ = await cheerio.load(htmlResult);
